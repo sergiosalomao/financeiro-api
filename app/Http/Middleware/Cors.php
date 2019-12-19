5d403e6a1e8402d46d95e4631646494fd598d0b0
Sergio Salomao <salomao.leite@fabricainfo.com>
2019-12-19 17:28:06 -0300

    ajustes

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -7,9 +7,11 @@ use Closure;
 class Cors
 {
     public function handle($request, Closure $next)
-{
-    return $next($request)
-        ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-}
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
+
+    }
 }
