639ad46a0ee644937767dc1d798ded6f8c002968
Sergio Salomao <salomao.leite@fabricainfo.com>
2019-12-20 18:24:12 -0300

    ajustes

--- app/Http/Controllers/TituloController.php
+++ app/Http/Controllers/TituloController.php
@@ -12,21 +12,27 @@ class TituloController extends Controller
 {
 
     public function index(Request $request, Titulo $titulo)
-    {      
-      
+    {
         $dados = $titulo->newQuery();
-        
+        if ($request->filled('informacoes')) {
+            $dados = $dados->get();
+            return $this->getInformacoes($dados);
+        }
+
+
+        $dados = $titulo->newQuery();
+
         if ($request->filled('status'))  $dados->where('status', 'like', '%' . $request->status . '%');
         if ($request->filled('conta_id'))  $dados->where('conta_id', 'like', '%' . $request->conta_id . '%');
         if ($request->filled('fluxo_id'))  $dados->where('fluxo_id', 'like', '%' . $request->fluxo_id . '%');
         if ($request->filled('cedente_id'))  $dados->where('cedente_id', 'like', '%' . $request->cedente_id . '%');
         if ($request->filled('valor'))     $dados->where('valor', 'like', '%' . $request->valor . '%');
-        
-        if ($request->filled('datainicio','datafinal'))   
-         $dados->whereBetween('vencimento', [implode('-', array_reverse(explode('/', $request->datainicio))), implode('-', array_reverse(explode('/', $request->datafinal)))])->get();
 
-        $dados = $dados->with(['conta', 'fluxo','cedente'])->orderBy('vencimento','asc')->get();
-        
+        if ($request->filled('datainicio', 'datafinal'))
+            $dados->whereBetween('vencimento', [implode('-', array_reverse(explode('/', $request->datainicio))), implode('-', array_reverse(explode('/', $request->datafinal)))])->get();
+
+        $dados = $dados->with(['conta', 'fluxo', 'cedente'])->orderBy('vencimento', 'asc')->get();
+
         return response()->json($dados, 200);
     }
 
@@ -46,7 +52,7 @@ class TituloController extends Controller
     public function show($id)
     {
         try {
-            $dados = Titulo::with(['conta', 'fluxo','cedente'])->find($id);
+            $dados = Titulo::with(['conta', 'fluxo', 'cedente'])->find($id);
             if (empty($dados)) {
                 return response('registro nao encontrado.', 200);
             }
@@ -64,13 +70,11 @@ class TituloController extends Controller
             $dados = Titulo::findOrFail($id);
             $dados->update($param);
 
-            
-            if ($dados->status == 'Pago')
-            $this->criarLancamento($dados);
-            else if ($dados->status == 'Aberto')
-            $this->apagarLancamento($id);
 
-           
+            if ($dados->status == 'Pago')
+                $this->criarLancamento($dados);
+            else if ($dados->status == 'Aberto')
+                $this->apagarLancamento($id);
         } catch (Exception $e) {
             return response('Erro:' . $e->getMessage(), 500);
         }
@@ -79,16 +83,14 @@ class TituloController extends Controller
 
     private function apagarLancamento($tituloId)
     {
-        $dados = Lancamento::where('titulo_id',$tituloId);
-        $dados->delete();     
+        $dados = Lancamento::where('titulo_id', $tituloId);
+        $dados->delete();
     }
 
     private function criarLancamento($dados)
     {
         try {
-            
             $lanc = new Lancamento;
-            
             $lanc->data_lancamento = date("d/m/Y");
             $lanc->tipo = $dados->tipo;
             $lanc->conta_id = $dados->conta_id;
@@ -111,10 +113,60 @@ class TituloController extends Controller
             if (empty($dados)) {
                 return response('registro nao encontrado.', 200);
             }
-            $dados->delete();           
+            $dados->delete();
             return response()->json(['Dados excluidos', 'DADOS' => $dados], 200);
         } catch (Exception $e) {
             return response('Erro:' . $e->getMessage(), 500);
         }
+    }
+    public function getInformacoes($dados)
+    {
+
+
+
+        $total['totalPago'] = 0;
+        $total['totalApagar'] = 0;
+        $total['totalRecebido'] = 0;
+        $total['totalAReceber'] = 0;
+
+
+        foreach ($dados as $dado) {
+            if (($dado['status'] == "Pago") && ($dado['tipo'] == "Debito")) {
+                $total['totalPago'] += $dado['valor'];
+            }
+            if (($dado['status'] == "Aberto") && ($dado['tipo'] == "Debito")) {
+                $total['totalApagar'] += $dado['valor'];
+            }
+
+            if (($dado['status'] == "Pago") && ($dado['tipo'] == "Credito")) {
+                $total['totalRecebido'] += $dado['valor'];
+            }
+
+            if (($dado['status'] == "Aberto") && ($dado['tipo'] == "Credito")) {
+                $total['totalAReceber'] += $dado['valor'];
+            }
+        }
+
+        $totalArray = array(
+            "totalApagar" => [
+                "text" => "Total a Pagar",
+                "value" => $total['totalApagar'],
+            ],
+            "totalAReceber" => [
+                "text" => "Total a Receber",
+                "value" => $total['totalAReceber'],
+            ],
+            "totalPago" => [
+                "text" => "Total Pago",
+                "value" => $total['totalPago'],
+            ],
+            "totalRecebido" => [
+                "text" => "Total Recebido",
+                "value" => $total['totalRecebido'],
+            ],
+
+        );
+
+        return $totalArray;
     }
 }
